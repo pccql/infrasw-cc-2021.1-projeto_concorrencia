062326929d6ee88526c21f77d271d7b24f9d0c25
Pedro Queiroz <pccql@cin.ufpe.br>
2021-11-09 18:04:48 -0300

    feat(main): Player file structure

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,47 @@
+import java.awt.event.ActionListener;
+import ui.PlayerWindow;
+
+
 public class Player {
     public Player() {
+
+        ActionListener buttonListenerPlayNow = e -> start();
+        ActionListener buttonListenerRemove = e -> remove();
+        ActionListener buttonListenerAddSong = e -> {
+            addSongWindow = new AddSongWindow(
+                createID(), 
+                buttonListenerAddSong, 
+                window.getAddSongWindowListener()
+                );
+        };
+
+        PlayerWindow window = new PlayerWindow(
+            buttonListenerAddSong,
+            buttonListenerPlayNow, 
+            buttonListenerRemove
+        );
+
+        ActionListener actionListener = e -> {};
+
+        AddSongWindow addSongWindow = new AddSongWindow(
+            createID(), 
+            buttonListenerAddSong, 
+            window.getAddSongWindowListener()
+            );
+
     }
+
+    int songID = 1;
+
+    public int createID() {
+        songID += 1;
+        return songID;
+    }
+
+    public void start() {};
+
+    public void remove() {};
+
+    public void add() {};
 }
 
