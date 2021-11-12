13f6cc6248c908adf56572a24a3243a55044ad4c
Pedro Queiroz <pccql@cin.ufpe.br>
2021-11-12 18:58:58 -0300

    feat(main): create player base structure

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,10 @@
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 import ui.PlayerWindow;
+import ui.AddSongWindow;
 
 
 public class Player {
@@ -7,41 +12,94 @@ public class Player {
 
         ActionListener buttonListenerPlayNow = e -> start();
         ActionListener buttonListenerRemove = e -> remove();
-        ActionListener buttonListenerAddSong = e -> {
-            addSongWindow = new AddSongWindow(
-                createID(), 
-                buttonListenerAddSong, 
-                window.getAddSongWindowListener()
-                );
+        ActionListener buttonListenerAddSong = e -> add();
+        ActionListener buttonListenerPlayPause = e -> playpause();
+        ActionListener buttonListenerStop = e -> stop();
+        ActionListener buttonListenerNext = e -> next();
+        ActionListener buttonListenerPrevious = e -> prev();
+        ActionListener buttonListenerShuffle = e -> shuffle();
+        ActionListener buttonListenerRepeat = e -> repeat();
+        MouseListener scrubberListenerClick = new MouseListener(){
+            @Override
+            public void mouseClicked(MouseEvent e) {}
+
+            @Override
+            public void mouseReleased(MouseEvent e){}
+
+            @Override
+            public void mousePressed(MouseEvent e){}
+
+            @Override
+            public void mouseEntered(MouseEvent e){}
+
+            @Override
+            public void mouseExited(MouseEvent e){}
         };
 
+        MouseMotionListener scrubberListenerMotion = new MouseMotionListener(){
+            @Override
+            public void mouseDragged(MouseEvent e) {}
+
+            @Override
+            public void mouseMoved(MouseEvent e){}
+
+        };
+
+        String windowTitle = "Player de Música";
+
+
+
         PlayerWindow window = new PlayerWindow(
-            buttonListenerAddSong,
-            buttonListenerPlayNow, 
-            buttonListenerRemove
+                buttonListenerPlayNow,
+                buttonListenerRemove,
+                buttonListenerAddSong,
+                buttonListenerPlayPause,
+                buttonListenerStop,
+                buttonListenerNext,
+                buttonListenerPrevious,
+                buttonListenerShuffle,
+                buttonListenerRepeat,
+                scrubberListenerClick,
+                scrubberListenerMotion,
+                windowTitle,
+                getQueueasArray()
         );
+
+
+        String songID = "1";
 
         ActionListener actionListener = e -> {};
 
-        AddSongWindow addSongWindow = new AddSongWindow(
-            createID(), 
-            buttonListenerAddSong, 
-            window.getAddSongWindowListener()
-            );
+        AddSongWindow addSongWindow = new AddSongWindow(songID, buttonListenerAddSong, window.getAddSongWindowListener());
+
+
 
     }
 
-    int songID = 1;
+    String[][] array = new String[100][100];
 
-    public int createID() {
-        songID += 1;
-        return songID;
-    }
 
     public void start() {};
 
     public void remove() {};
 
     public void add() {};
-}
 
+    public void playpause() {};
+
+    public void stop() {};
+
+    public void next() {};
+
+    public void prev() {};
+
+    public void shuffle() {};
+
+    public void repeat() {};
+
+    public String[][] getQueueasArray() {
+
+        return array;
+    };
+
+}
